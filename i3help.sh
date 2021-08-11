6f4aec9a1aebf4fb8dfdd029a8578262873426d0
flxndn <felix.anadon@gmail.com>
2021-08-11 09:20:28 +0200

    Añadido para los ordenadores con i3

---
+++ i3help.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat <<HELP
+* i3help.sh
+	* Descripción
+		Chuleta para usar i3.
+	* Trucos
+		* Cambiar la red wifi
+			Usar '''nmtui'''
+		* Fondo de escritorio
+			Restore your settings on startup by adding the following line to your ~/.i3/config:
+			> exec --no-startup-id nitrogen --restore
+		* Cerrar la sesión
+			press Mod+Shift+e 
+
+HELP
